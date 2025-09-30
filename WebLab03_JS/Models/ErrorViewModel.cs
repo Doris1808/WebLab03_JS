c0d93e5a36507d33f5f40eec6ad45fbcb3fb90ee
Doris1808 <jasonluong1808@gmail.com>
2025-09-30 09:43:06 +0700

    WebLab03_JS - initial commit

---
+++ WebLab03_JS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebLab03_JS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
